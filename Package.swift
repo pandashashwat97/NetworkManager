9a4569728c12d41b0a276dd81e83080c39f8b5b3
pandashashwat97 <pandashashwat97@gmail.com>
2024-05-07 23:43:19 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NetworkManager",
+    products: [
+        .library(
+            name: "NetworkManager",
+            targets: ["NetworkManager"]),
+    ],
+    targets: [
+        .target(
+            name: "NetworkManager")
+    ]
+)
---
+++ Sources/NetworkManager/NetworkManager.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+public class NetworkManager{
+
+    public init(){}
+    
+    public func getData(link:String, completion: @escaping (Data)->Void){
+        if let url = URL(string: link){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url){
+                (data, response, error) in
+                if error != nil{
+                    print("Something went wrong")
+                    print("error: \(String(describing: error))")
+                    return
+                }
+                if let safeData = data{
+                    completion(safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+}
+
